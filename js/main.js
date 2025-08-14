952839f293618be72a065cbc6d00a24b958377c0
pokisnuto <krizicsara@gmail.com>
2025-08-14 23:03:22 +0200

    main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('I love you! Happy 1 year anniversary!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
